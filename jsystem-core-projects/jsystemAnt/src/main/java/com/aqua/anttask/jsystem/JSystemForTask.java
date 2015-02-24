10bc86160d38e283c13556279607f2b75ba9e66f
itaiag <itai.agmon@gmail.com>
2015-02-24 15:19:54 +0200

    Work in progress

--- jsystem-core-projects/jsystemAnt/src/main/java/com/aqua/anttask/jsystem/JSystemForTask.java
+++ jsystem-core-projects/jsystemAnt/src/main/java/com/aqua/anttask/jsystem/JSystemForTask.java
@@ -3,6 +3,8 @@
  */
 package com.aqua.anttask.jsystem;
 
+import jsystem.utils.StringUtils;
+
 import org.apache.tools.ant.BuildException;
 
 /**
@@ -21,11 +23,36 @@ public class JSystemForTask extends PropertyReaderTask {
 		if (!JSystemAntUtil.doesContainerHaveEnabledTests(getUuid())) {
 			return;
 		}
-		setList(getParameterFromProperties("list", DEFAULT_LIST_VALUE));
-		setParam(getParameterFromProperties("loop value", DEFAULT_PARAM_VALUE));
+		if (!isScenarioCreatedByOlderVersion()) {
+			setParametersFromProperties();
+		}
 		super.execute();
 	}
 
+	private void setParametersFromProperties() {
+		setList(getParameterFromProperties("list", DEFAULT_LIST_VALUE));
+		setParam(getParameterFromProperties("loop value", DEFAULT_PARAM_VALUE));
+	}
 
+	/**
+	 * Checks if the scenario was created with JSystem in version 6.0.01 or
+	 * older. In this case the parameters were saved in the XML file instead of
+	 * in the properties file.
+	 * 
+	 * @return true if and only if the parameters in the XML file is different
+	 *         from the default ones.
+	 */
+	private boolean isScenarioCreatedByOlderVersion() {
+		final String list = getList();
+		if (!StringUtils.isEmpty(list) && !list.equals(DEFAULT_LIST_VALUE)) {
+			return true;
+		}
+
+		final String param = getParam();
+		if (!StringUtils.isEmpty(param) && !param.equals(DEFAULT_PARAM_VALUE)) {
+			return true;
+		}
+		return false;
+	}
 
 }
